2ce3106481535ecc15f6a809bd50ec7c4abdf67e
ViktorBoem <148382575+ViktorBoem@users.noreply.github.com>
2025-03-04 01:45:03 +0200

    Update Program.cs

--- AsmInsertion/Program.cs
+++ AsmInsertion/Program.cs
@@ -8,27 +8,18 @@ namespace AsmInsertion
 {
     internal class Program
     {
-        // Відключення перевірки безпеки пам'яті CLR при виклику некерованого коду
         [SuppressUnmanagedCodeSecurity]
-        // Атрибут для делегатів, які вказують на некеровані функції, прописує "C" декларацію
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        // Делегат для виклику функції з некерованим асемблерним кодом
         private delegate int AssemblyAddFunction(int x, int y);
 
-        // Вказує, що функція "VirtualProtectEx" імпортується з "kernel32.dll"
-        // та змінює параметри доступу до віртуальної пам'яті певного процесу
         [DllImport("kernel32.dll")]
         private static extern bool VirtualProtectEx(IntPtr hProcess, IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
 
         static void Main(string[] args)
         {
-            // отримуємо об'єкт, що представляє поточний процес
             var process = System.Diagnostics.Process.GetCurrentProcess();
-            // зміння для збереження результату асемблерної функції
             int returnValue;
 
-
-            // масиву байтів, що містить асемблерний код
             byte[] assembledCode =
             {
                 0x55,               // push ebp            
@@ -41,32 +32,23 @@ namespace AsmInsertion
                 0xC3                // ret                  
             };
 
-
-            // дозволяє використовувати "небезпечний" код, вказівники як у C/С++
             unsafe
             {
-                // фіксація байт масиву у пам'яті, щоб збирач сміття його не перемістив
                 fixed (byte* ptr = assembledCode)
                 {
-                    // приведення до керованого вказівника
                     var memoryAddress = (IntPtr)ptr;
 
-                    // зміна атрибуту ділянки пам'яті, яка виділені під "memoryAddress" на читання та запис
                     if (!VirtualProtectEx(process.Handle, memoryAddress, (UIntPtr)assembledCode.Length, 0x40, out uint _))
                     {
-                        // в разі не успішної зміни атрибутів генерується вийняток
                         throw new Win32Exception();
                     }
 
-                    //сторюється кероване прдеставлення асемблерної функції, отриманням делегату який на неї вказує
                     var myAssemblyFunction = Marshal.GetDelegateForFunctionPointer<AssemblyAddFunction>(memoryAddress);
-                    //виклик асемблерної функції
                     returnValue = myAssemblyFunction(2, 1);
 
                 }
             }
 
-            //вивід результату виконання асемблерної функції
             Console.WriteLine($"мій бал з АККМ: {returnValue}");
         }
     }
